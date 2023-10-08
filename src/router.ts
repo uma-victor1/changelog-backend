9e65f46a3e5a92427adbc0a66ae79b0e0046d60d
Victor Uma <umavictor11@gmail.com>
2023-10-08 11:08:40 +0100

    added routes

---
+++ src/router.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express'
+
+const router = Router()
+
+/**
+ * product routes
+ */
+router.get('/product', (req, res, next) => {
+  res.status(200)
+  res.json({ message: 'hello' })
+})
+router.get('/product/:id', () => {})
+router.put('/product/:id', () => {})
+router.post('/product/', () => {})
+router.delete('/product/:id', () => {})
+
+/**
+ * Update
+ */
+
+router.get('/update', () => {})
+router.get('/update/:id', () => {})
+router.put('/update/:id', () => {})
+router.post('/update/', () => {})
+router.delete('/update/:id', () => {})
+
+/**
+ * update point
+ */
+
+router.get('/updatepoint', () => {})
+router.get('/updatepoint/:id', () => {})
+router.put('/updatepoint/:id', () => {})
+router.post('/updatepoint/', () => {})
+router.delete('/updatepoint/:id', () => {})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
 import express from 'express'
+import router from './router'
 
 const app = express()
+app.use('/api', router)
 
 
-
-app.get('/', (req, res) => {
-  console.log('uhmmm sucks')
-  res.status(200)
-  res.json({ message: 'hello' })
-})
 
 export default app
